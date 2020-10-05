c19b96930b3513cdd17c5573edf8c1a6c8f1d48c
Green Learner <greenlearner01@users.noreply.github.com>
2020-10-05 20:44:41 +0530

    file tree walker

--- java/code-stuff/src/com/greenlearner/files/FileTreeWalker.java
+++ java/code-stuff/src/com/greenlearner/files/FileTreeWalker.java
@@ -1,6 +1,10 @@
 package com.greenlearner.files;
 
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author - GreenLearner(https://www.youtube.com/c/greenlearner)
@@ -8,5 +12,27 @@ import java.io.IOException;
 public class FileTreeWalker {
     public static void main(String[] args) throws IOException {
         String directoryPath = "c:/temp";
+
+        List<String> files = new ArrayList<>();
+
+        Path path = Paths.get(directoryPath);
+
+        Files.walkFileTree(path,new SimpleFileVisitor<>(){
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.println("directory - " + dir.toAbsolutePath());
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                files.add(file.toAbsolutePath().toString());
+                System.out.println("file - " + file.toAbsolutePath());
+                return super.visitFile(file, attrs);
+            }
+        });
+        System.out.println(files);
     }
+
+
 }
--- java/code-stuff/src/com/greenlearner/files/FileTreeWalkerNormalWay.java
+++ java/code-stuff/src/com/greenlearner/files/FileTreeWalkerNormalWay.java
@@ -8,5 +8,26 @@ import java.io.File;
 public class FileTreeWalkerNormalWay {
     public static void main(String[] args) {
         String directoryPath = "c:/temp";
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
+        for(File file : files){
+            printFileName(file, 0);
+        }
     }
+
+    private static void printFileName(File file, int level) {
+        for(int i=0; i< level; i++){
+            System.out.print("\t");
+        }
+        if(file.isFile()){
+            System.out.println(file.getName());
+        }else{
+            System.out.println("$$$ Directory Name - " + file.getAbsolutePath());
+            File[] files = file.listFiles();
+            for(File file1 : files){
+                printFileName(file1, level+1);
+            }
+        }
+    }
+
 }
